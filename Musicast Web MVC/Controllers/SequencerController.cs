a6e1a87bf2318887f5825b60e6486d445efcb7c4
User <User@WIN-69H0CTIA700>
2016-09-12 00:37:23 +0300

    Добавьте файлы проекта.

---
+++ Musicast Web MVC/Controllers/SequencerController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Musicast_Web_MVC.Controllers
+{
+    public class SequencerController : Controller
+    {
+        // GET: Sequencer
+        public ActionResult Sequencer()
+        {
+            ViewBag.Message = "Your sequencer page.";
+
+            return View();
+        }
+    }
+}
---
+++ Musicast Web MVC/Scripts/pattern.js
@@ -0,0 +1,68 @@
+﻿(function () {
+
+    String.prototype.format = function (dataObject) {
+        return this.replace(/{(.+)}/g, function (match, propertyName) {
+            return dataObject[propertyName];
+        });
+    };
+
+    function getData() {
+        $.getJSON("/api/game", null, displayData);
+    };
+
+    function displayData(data) {
+        var target = $("#patternTable tbody");
+        target.empty();
+        var template = $("#rowTemplate");
+        data.forEach(function (dataObject) {
+            target.append(template.html().format(dataObject));
+        });
+        $(target).find("button").click(function (e) {
+            $("*.errorMsg").remove();
+            $("*.error").removeClass("error");
+            var index = $(e.target).attr("data-id");
+            if ($(e.target).attr("data-action") == "delete") {
+                deleteData(index);
+            } else {
+                var gameData = { gameID: index };
+                $(e.target).closest('tr').find('input')
+                    .each(function (index, inputElem) {
+                        gameData[inputElem.name] = inputElem.value;
+                    });
+                updateData(index, gameData);
+            }
+            e.preventDefault();
+        });
+    }
+
+    function deleteData(index) {
+        $.ajax({
+            url: "/api/game/" + index,
+            type: 'DELETE',
+            success: getData
+        });
+    }
+
+    function updateData(index, gameData) {
+        $.ajax({
+            url: "/api/game/" + index,
+            type: 'PUT',
+            data: gameData,
+            success: getData,
+            error: function (jqXHR, status, error) {
+                var errorRow = $("button[data-id=" + index + "]").closest("tr");
+                errorRow.find("*").addClass("error");
+                var errData = JSON.parse(jqXHR.responseText);
+                for (var i = 0; i < errData.length; i++) {
+                    errorRow.after("<tr class='errMsg error'><td/><td colspan=3>"
+                        + errData[i] + "</td></tr>");
+                }
+            }
+        });
+    }
+
+    $(document).ready(function () {
+        getData();
+    });
+
+})();
