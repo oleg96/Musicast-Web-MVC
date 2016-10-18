2d08bd8060bc4abc67cfc12ce4253aec167f74da
oleg96 <andoider87@gmail.com>
2016-10-18 19:18:13 +0300

    Added music generator page

---
+++ Musicast Web MVC/Controllers/MusicGenController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Musicast_Web_MVC.Controllers
+{
+    public class MusicGenController : Controller
+    {
+        // GET: MusicGen
+        public ActionResult MusicGen()
+        {
+            return View();
+        }
+    }
+}
